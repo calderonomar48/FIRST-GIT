535718b43c8819c1d9ac5adf068720536cf98101
OCALDERON - GMD <ocalderon@gmd.com.pe>
2012-03-09 22:24:33 -0500

    comiteando

--- file1.php
+++ file1.php
@@ -1,18 +1,6 @@
 <?php
 
-class file1_guitarrachay{
-	public __construct(){
-		var prueba = "";
-	}
-
-	public function listando_objects(){
-		return false;
-	}
-}
-
-
-$obj = new file1();
-$obj->listando_objects();
+echo "hola mudno php";
 
 
 ?>
